bdd1ead6924f33833efeb7031521aa0344d94374
Commander_Cobra <gregawscsa@gmail.com>
2021-06-22 19:48:22 -0400

    updated working coin market cap data

--- bot.py
+++ bot.py
@@ -14,19 +14,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
 
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
 
 # Second Test prints data from cmtest (Now need to export to discord)
 
@@ -35,15 +23,10 @@ result = subprocess.run(["python", "cmtest.py"], capture_output=True, encoding='
 
 cmc = result.stdout
 
-print(cmc)
-
-
 @bot.command(name='awf', help='Responds with CMC data for $AWF')
 async def cmc_data(ctx):
     awf = cmc    
-
-    response = random.choice(cmc)
-    await ctx.send(response)
+    await ctx.send(cmc)
 
 
 bot.run(TOKEN)
